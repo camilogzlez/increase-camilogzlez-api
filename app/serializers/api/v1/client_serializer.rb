b01f0bce10995825da2d6b4c441aded8efc6dbab
camilogzlez <camilogzlez@gmail.com>
2021-10-09 15:44:46 -0300

    Clients index implemented

---
+++ app/serializers/api/v1/client_serializer.rb
@@ -0,0 +1,18 @@
+module Api
+  module V1
+    class ClientSerializer < ApplicationSerializer
+      attributes :id,
+                 :email,
+                 :first_name,
+                 :last_name,
+                 :job,
+                 :country,
+                 :address,
+                 :zip_code,
+                 :phone,
+                 :external_id,
+                 :payments
+                
+    end
+  end
+end
